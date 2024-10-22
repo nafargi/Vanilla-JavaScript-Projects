5b0e45f0a6da5f5fc4c207f496ce6f31efdcc6c9
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-10-22 22:26:16 +0300

    regex edited properly

--- Form-Validator/app.js
+++ Form-Validator/app.js
@@ -14,7 +14,6 @@ function showSuccess(input) {
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
   }
-
 function checkEmail(input){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
