61f5372285ae2e40d8a57adcc4eb7c86be1f7b2a
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-10-24 23:43:19 +0300

    Update app.js

--- Movie Seat Booking/app.js
+++ Movie Seat Booking/app.js
@@ -7,7 +7,6 @@ const text = document.querySelector('.text');
 const text2 = document.querySelector('.text2');
 const occupiedSeat = document.querySelectorAll('.occupied');
 populateUI();
-
 let ticketPrice = +movieSelect.value;
 function setMovieData(movieIndex, moviePrice) {
   localStorage.setItem('selectedMovieIndex', movieIndex);
