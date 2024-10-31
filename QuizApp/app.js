3ba9a9d9e3ce468ba28688b807ad671cc7513f09
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-10-31 23:23:48 +0300

    Update app.js

--- QuizApp/app.js
+++ QuizApp/app.js
@@ -2,7 +2,6 @@ const form = document.querySelector('.quiz-form');
 const questions = document.querySelectorAll('.question');
 const result = document.querySelector('.result');
 const correctAnswers = ['D','B','C','B','D'];
-
 form.addEventListener('submit',(e) => {
     let score =0;
     e.preventDefault();
